a834c5230789363e84c2fea1be8d3676e1736ecb
Aayush Upadhyay <aayushu@gmail.com>
2014-02-16 16:47:59 -0500

    basic twilio code

--- wambam/website/run.py
+++ wambam/website/run.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from twilio.rest import TwilioRestClient
+
+twaccount = "AC032798eca07124939abd8352c516f86d"
+twtoken = "bbc20641c4fdb7e5a0ccc86b4fdefcfe"
+twilio_client = TwilioRestClient(twaccount, twtoken)
+
 app = Flask(__name__)
 
 flask_pos = []
@@ -20,6 +26,7 @@ def execute():
 @app.route("/confirmphone", methods=['POST'])
 def confirm():
     phone = request.form['phonenumber']
+    message = twilio_client.messages.create(to="+1"+phone, from_="+1954607-3879", body="Confirmed for WAMBAM!")
     return render_template('confirmation.html')
 
 if __name__ == "__main__":
