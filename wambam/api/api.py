951c357f809c1aa8180a297eb66225f3f49c89ad
adit <adit102695@gmail.com>
2014-02-26 14:54:58 -0500

    Started working on implementing logins

--- wambam/api/api.py
+++ wambam/api/api.py
@@ -2,7 +2,9 @@ import flask
 import flask.ext.sqlalchemy
 import flask.ext.restless
 import uuid
+import flask.ext.login
 
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 import schema
 
 def create_app():
@@ -24,6 +26,10 @@ def create_database(app):
     # Create the database tables.
     db.create_all()
 
+    a = schema.Account(email='hihihi', password_hash='')
+    db.session.add(a)
+    db.session.commit()
+
     return db
 
 
@@ -34,17 +40,30 @@ def create_api(app, db):
     # Create API endpoints, which will be available at /api/<tablename> by
     # default. Allowed HTTP methods can be specified as well.
     
-    manager.create_api(schema.Account, methods=['GET', 'POST', 'DELETE'])
+    manager.create_api(schema.Account, methods=['GET', 'POST', 'DELETE', 'PATCH'])
     manager.create_api(schema.Task, methods=['GET', 'POST', 'DELETE'])
     return manager
-        
+
 
 app = create_app()
+app.config['SECRET_KEY'] = 'WaMbAm'
+
 db = create_database(app)
 api_manager = create_api(app,db)
+login_manager = flask.ext.login.LoginManager()
+login_manager.init_app(app)
 
+def _token_loader(token):
+    try:
+        user = schema.Account.verify_auth_token(token)
+        return user
+    except:
+        pass
+
+login_manager.token_loader(_token_loader)
 
 @app.route('/')
+@flask.ext.login.login_required
 def hello():
     return 'Hello World'
 
@@ -58,7 +77,21 @@ def tasks_for_fulfiller(fulfiller):
     data = schema.Task.query.filter(schema.Task.fulfiller_accounts.any(schema.Account.id == fulfiller)).all()
     return flask.jsonify(items=data)
 
+@app.route('/api/token', methods=['POST'])
+def token_api():
 
-    
+    email = flask.request.values.get('email')
+    password = flask.request.values.get('password')
+
+    return email + ' ' + password
+    user = schema.Account.query.filter_by(email=email).filter_by(password_hash=password).first()
+    if user is None:
+        flask.abort(401)
+    else:
+        token = user.generate_auth_token()
+        
+    return token
+
+
 # start the flask loop
 app.run()
