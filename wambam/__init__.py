afbf3757ff23c73aedb36b90946cee937a37cec2
Aayush Upadhyay <aayushu@gmail.com>
2014-02-26 19:40:50 -0500

    updated reqs, wambam init

--- wambam/__init__.py
+++ wambam/__init__.py
@@ -3,7 +3,13 @@ import sys
 
 app = Flask(__name__)
 
-sys.path.append("wambam\\website");
-sys.path.append("wambam\\api");
+if sys.platform == 'win32':
+    sys.path.append("wambam\\website");
+    sys.path.append("wambam\\api");
+
+else:
+    sys.path.append("wambam/website");
+    sys.path.append("wambam/api");
+
 import api
 import run
