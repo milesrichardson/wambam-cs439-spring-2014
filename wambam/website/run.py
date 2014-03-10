07fba5dde42411a51641dad3609c25bdae11e972
mhopkins2 <michael.hopkins@yale.edu>
2014-03-10 14:51:32 -0400

    Setting up for heroku

--- wambam/website/run.py
+++ wambam/website/run.py
@@ -101,5 +101,4 @@ def confirm():
     return render_template('confirmation.html')
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
