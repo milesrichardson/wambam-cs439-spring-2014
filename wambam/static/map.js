7ce8089ef674ba88a016e0d9ad993b5149c29536
adit <adit102695@gmail.com>
2014-03-18 01:36:51 -0400

    adapted the home screen for mobile phones

---
+++ wambam/static/map.js
@@ -0,0 +1,48 @@
+function setMapSize() {
+    var mapHeight = $("body").height() - $("#buttonbar").height() - $("#header").height() - 60;
+    $("#map-canvas").height(mapHeight);
+}
+
+$(window).on('resize', function() { setMapSize(); });
+
+var map;
+function initialize() {
+    var mapOptions = {
+        zoom: 15,
+        center: new google.maps.LatLng(41.3111, -72.9267)
+    };
+
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+                              mapOptions);
+
+    map.setOptions({styles: [
+	{
+	    featureType: "poi",
+            elementType: "labels",
+	    stylers: [
+	        { visibility: "off" }
+	    ]
+	}
+    ]});
+
+    var marker = null;
+
+    google.maps.event.addListener(map, 'click', function(e) {
+        if (marker == null) {
+            marker = new google.maps.Marker({
+		position: e.latLng,
+		map: map,
+		title: 'Request Location'
+            });
+            $("#create_task_button").show(); 
+        }
+        else {
+            marker.setPosition(e.latLng);
+        }
+        $("#lat").val(e.latLng.lat());
+        $("#lng").val(e.latLng.lng())
+    });
+    setMapSize();
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
